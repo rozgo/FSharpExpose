f827005a1ea16e601c4c3555225bd0517f7738cb
rozgo <alex.rozgo@gmail.com>
2015-04-30 18:29:34 -0600

    more

--- Program.fs
+++ Program.fs
@@ -70,42 +70,132 @@ type CmdResultCompletion = {
     Data : (string * string * string) []
 }
 
+type internal PrintingAgent () =
+    let agent = MailboxProcessor.Start (fun agent ->
+        let rec loop () = async {
+            let! (msg: Choice<string,AsyncReplyChannel<unit>>) = agent.Receive()
+            match msg with
+            | Choice1Of2 (s: string) -> Console.WriteLine s; return! loop ()
+            | Choice2Of2 ch -> ch.Reply ()
+        }
+        loop ()
+    )
+
+    member x.WriteLine(s) = agent.Post (Choice1Of2 s)
+
 
 [<EntryPoint>]
 let main argv = 
 //    printfn "%A" argv
 
-    let projectFile = "/Users/rozgo/Projects/fsharp-edit/Test1/Test1.fsproj"
-    let sourceFile = "/Users/rozgo/Projects/fsharp-edit/Test1/Program.fs"
 
-//    let projectFile = "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/SingleAppDemo.fsproj"
-//    let sourceFile = "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/Main.fs"
-//
-    let sourceText = (new StreamReader(sourceFile)).ReadToEnd ()
+    let agent = new PrintingAgent ()
+
+    use _logFile = new StreamWriter ("/Users/rozgo/Documents/fsharp-expose.log", false)
+
+    let log (s:string) =
+        _logFile.Write s
+        _logFile.Flush ()
+
+//    let projectFile = "/Users/rozgo/Projects/fsharp-edit/Test1/Test1.fsproj"
+//    let sourceFile = "/Users/rozgo/Projects/fsharp-edit/Test1/Program.fs"
+
+    let projectFile = "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/SingleAppDemo.fsproj"
+    let sourceFile = "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/Main.fs"
+////
+//    let sourceText = (new StreamReader(sourceFile)).ReadToEnd ()
 
     let checker = FSharpChecker.Create ()
+
+    Directory.SetCurrentDirectory ("/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo")
+
+    log (sprintf "PWD: %s\n" (Directory.GetCurrentDirectory()))
+
+    //SourceCodeServices.FSharpPro
+
+    try
+
+        let p =
+            FSharpProjectFileInfo.Parse (projectFile,
+                [
+                ("Configuration", "Debug")
+//                ("Target", "Build")
+
+                ("MSBuildBinPath", "/Library/Frameworks/Mono.framework/Versions/4.0.0/lib/mono/4.5")
+                ("MSBuildExtensionsPath", "/Library/Frameworks/Mono.framework/External/xbuild")
+                ("MSBuildExtensionsPath32", "/Library/Frameworks/Mono.framework/External/xbuild")
+                ("MSBuildExtensionsPath64", "/Library/Frameworks/Mono.framework/External/xbuild")
+                ("MSBuildProjectDefaultTargets", "Build")
+                ("MSBuildProjectDirectory", "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo")
+                ("MSBuildProjectFile", "SingleAppDemo.fsproj")
+                ("MSBuildProjectFullPath", "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/SingleAppDemo.fsproj")
+                ("MSBuildProjectName", "SingleAppDemo")
+                ("MSBuildToolsVersion", "4.0")
+                ("MSBuildToolsPath", "/Library/Frameworks/Mono.framework/Versions/4.0.0/lib/mono/4.5")
+                ("MSBuildToolsRoot", "/Library/Frameworks/Mono.framework/Versions/4.0.0/lib/mono")
+//                ("TargetFrameworkIdentifier", "Xamarin.iOS")
+//                ("TargetFrameworkIdentifier", "")
+                ("FSharpTargets", "/Library/Frameworks/Mono.framework/Versions/4.0.0/lib/mono/4.5/Microsoft.FSharp.Targets")
+//                ("Platform", "iPhoneSimulator")
+//                ("MtouchArch", "i386")
+
+//                ("MtouchDebug", "true")
+//                ("MtouchLink", "None")
+//                ("MtouchProfiling", "true")
+//                ("MtouchTargetsEnabled", "true")
+//                ("OutputPath", "bin\iPhoneSimulator\Debug")
+//                ("OutputType", "Exe")
+//                ("ProjectGuid", "{0846E69E-9C79-44A9-B2FF-98DEFADDFBB9}")
+//                ("ProjectTypeGuids", "{FEACFBD2-3405-455C-9665-78FE426C6842};{F2A71F9B-5D33-465A-A702-920D77279786}")
+//                ("-r", "/Library/Frameworks/Mono.framework/External/xbuild/Xamarin/iOS/Xamarin.iOS.Tasks.dll")
+                ], false)
+
+        log (sprintf "LogOutput: %A\n" p.LogOutput)
+        log (sprintf "ProjectReferences: %A\n" p.ProjectReferences)
+        log (sprintf "Options: %A\n" p.Options)
+        log (sprintf "CompileFiles: %A\n" p.CompileFiles)
+        log (sprintf "FrameworkVersion: %A\n" p.FrameworkVersion)
+        log (sprintf "References: %A\n" p.References)
+        log (sprintf "OtherFiles: %A\n" p.OtherFiles)
+        log (sprintf "OutputPath: %A\n" p.OutputPath)
+        log (sprintf "OutputFile: %A\n" p.OutputFile)
+
+    with
+    | e -> log (sprintf "%s\n" (e.ToString()))
 
     let projectOptions = {
         IsIncompleteTypeCheckEnvironment = false
         LoadTime = DateTime.Now
-        OtherOptions =
-            [|
-                "--noframework"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/FSharp.Core.dll"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/mscorlib.dll"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.dll"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.Core.dll"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.Xml.dll"
-//                "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll"
-            |]
+        OtherOptions = Array.ofList (List.append [] [
+            "--simpleresolution"
+            "--noframework"
+            "--out:/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/bin/iPhoneSimulator/Debug/SingleAppDemo.exe"
+            "--fullpaths"
+            "--flaterrors"
+            "--target:exe"
+            "--define:__UNIFIED__"
+            "--define:__MOBILE__"
+            "--define:__IOS__"
+            "--define:DEBUG"
+            "--define:ENABLE_TEST_CLOUD"
+            "--debug+"
+            "--optimize-"
+            "--tailcalls-"
+//            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/FSharp.Core.dll"
+//            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/mscorlib.dll"
+//            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.dll"
+//            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.Core.dll"
+//            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/System.Xml.dll"
+            "-r:/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll"
+                ])
         ProjectFileName = projectFile
-        ProjectFileNames =
+        ProjectFileNames = //[||]//Array.ofList p.CompileFiles
             [|
-                "/Users/rozgo/Projects/fsharp-edit/Test1/FileTwo.fs"
-                "/Users/rozgo/Projects/fsharp-edit/Test1/Program.fs"
-//                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/AppDelegate.fs"
-//                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/ViewController.fs"
-//                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/Main.fs"
+//                "/Users/rozgo/Projects/fsharp-edit/Test1/FileTwo.fs"
+//                "/Users/rozgo/Projects/fsharp-edit/Test1/Program.fs"
+                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/AppDelegate.fs"
+                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/ViewController.fs"
+                "/Users/rozgo/Projects/SingleAppDemo/SingleAppDemo/Main.fs"
             |]
         ReferencedProjects = [||]
         UnresolvedReferences = None
@@ -171,12 +261,16 @@ let main argv =
 //        return sourceCode.ToString ()
 //    }
 
+
+
     let readLineCount = ref 0
  
     let stdin = Console.OpenStandardInput ()
 
     let handleCommands () = async {
-//        let! projectOptions = checker.GetProjectOptionsFromScript (sourceFile, sourceText, DateTime.Now)
+
+        let options = checker.GetProjectOptionsFromProjectFile (projectFile, [("Configuration", "Debug"); ("FSharpTargets", "/Library/Frameworks/Mono.framework/Versions/4.0.0/lib/mono/4.5/Microsoft.FSharp.Targets")], DateTime.Now)
+        log (sprintf "projectOptions:\n%A\n" options)
 //        printfn "%s" (JsonConvert.SerializeObject ({CmdProjectOptions.Kind = "debug"; Log = projectOptions}))
 
         let allFileCheckResults = new Dictionary<string, FSharpCheckFileResults> ()
@@ -186,15 +280,19 @@ let main argv =
         for line in readLines stdin do
 
 //            printfn "IN COUNT: %i" readLineCount.Value
-            printfn "LINE: %s" line
+            log (sprintf "INPUT %i:\n%s\n" readLineCount.Value line)
             readLineCount.Value <- readLineCount.Value + 1
 
             match line with
-            | CmdRegex.Project project -> 
+            | CmdRegex.Project project ->
+
+                //let! options = checker.Get
+
                 let! projectResults = checker.ParseAndCheckProject (projectOptions)
                 let json = JsonConvert.SerializeObject ({CmdResultError.Kind = "errors"; Data = projectResults.Errors})
-                printfn "%s" json
-//                printfn "PROJECT RESULTS %s" projectResults.Errors
+                agent.WriteLine (sprintf "%s" json)
+                log (sprintf "projectResults.Errors:\n%A\n" projectResults.Errors)
+//                log (sprintf "%A\n" projectResults.)
                 ()
             | CmdRegex.Parse file ->
 
@@ -206,6 +304,7 @@ let main argv =
                     let flat =
                         Array.fold (fun (sw:StringWriter) line -> sw.Write (line + "\n"); sw ) (new StringWriter ()) lines
                     flat.ToString ()
+                log (sprintf "Source:\n*******************\n%s\n*******************\n" source)
 
 //                printfn "MATCH FILE %s" file
 //                let! source = readSource stdin
@@ -214,13 +313,18 @@ let main argv =
                 let! fileParseResults = checker.ParseFileInProject (file, source, projectOptions)
                 let! fileAnswer = checker.CheckFileInProject (fileParseResults, file, 0, source, projectOptions)
 
+//                let json = JsonConvert.SerializeObject ({CmdResultError.Kind = "errors"; Data = fileParseResults.Errors})
+//                agent.WriteLine (sprintf "%s" json)
+//                log (sprintf "fileParseResults.Errors:\n%A\n" fileParseResults.Errors)
+
                 match fileAnswer with
                 | FSharpCheckFileAnswer.Succeeded fileCheckResults ->
                     allFileCheckResults.[file] <- fileCheckResults
                     allFileSources.[file] <- lines
                     allFileParseResults.[file] <- fileParseResults
                     let json = JsonConvert.SerializeObject ({CmdResultError.Kind = "errors"; Data = fileCheckResults.Errors})
-                    printfn "%s" json
+                    agent.WriteLine (sprintf "%s" json)
+                    log (sprintf "fileCheckResults.Errors:\n%A\n" fileCheckResults.Errors)
                 | FSharpCheckFileAnswer.Aborted ->
                     ()
 
@@ -234,8 +338,10 @@ let main argv =
                 | Some (tip, (x, y)) ->
                     let tipText = TipFormatter.formatTip tip
                     let json = JsonConvert.SerializeObject ({CmdResultTooltip.Kind = "tooltip"; Data = tipText})
-                    printfn "%s" json
-                | None -> ()
+                    agent.WriteLine (sprintf "%s" json)
+                | None ->
+                    let json = JsonConvert.SerializeObject ({CmdResultTooltip.Kind = "tooltip"; Data = "No tooltip available."})
+                    agent.WriteLine (sprintf "%s" json)
 
             | CmdRegex.Completion (file, prefix, row, column) ->
                 let fileCheckResults = allFileCheckResults.[file]
@@ -283,7 +389,8 @@ let main argv =
 //                    info.Items.[0].Name
                     //printfn "COMPLETION DOC: %s" doc
                     let json = JsonConvert.SerializeObject ({CmdResultCompletion.Kind = "completion"; Data = decls})
-                    printfn "%s" json
+                    agent.WriteLine (sprintf "%s" json)
+                    log (sprintf "%A\n" decls)
                     ()
                     
 //                | Some ((FSharpToolTipText tips), (x, y)) ->
@@ -307,12 +414,13 @@ let main argv =
             ()
     }
 
+
     try
         handleCommands ()
         |> Async.RunSynchronously
         |> ignore
     with
-    | e -> printfn "%s" (e.ToString())
+    | e -> log (sprintf "%s\n" (e.ToString()))
 
 
 
